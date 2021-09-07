3be5a399eb908a1e0be890ddd713c940e769457f
UA CR <blazer5@email.arizona.edu>
2021-09-06 23:24:27 -0700

    fixed typ in idb

--- public/js/idb.js
+++ public/js/idb.js
@@ -54,7 +54,7 @@ function uploadTrans() {
     getAll.onsuccess = function () {
         // if there was data in indexedDb's store, let's send it to the api server
         if (getAll.result.length > 0) {
-            fetch('/api/tansaction', {
+            fetch('/api/transaction', {
                 method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
